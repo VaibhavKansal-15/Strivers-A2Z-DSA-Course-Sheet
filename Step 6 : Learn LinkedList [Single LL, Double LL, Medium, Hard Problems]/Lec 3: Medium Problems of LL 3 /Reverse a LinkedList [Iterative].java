1be1a5ab2d0b4f3df0a4e40b21d039592e8d6911
Vaibhav Kansal <102716729+VaibhavKansal-15@users.noreply.github.com>
2025-03-04 19:49:09 +0530

    Create Reverse a LinkedList [Iterative].java

---
+++ Step 6 : Learn LinkedList [Single LL, Double LL, Medium, Hard Problems]/Lec 3: Medium Problems of LL 3 /Reverse a LinkedList [Iterative].java
@@ -0,0 +1,15 @@
+class Solution {
+  
+    public ListNode reverseList(ListNode head) {
+        ListNode prev=null;
+        ListNode next=null;
+        ListNode current=head;
+        while(current!=null){
+            next=current.next;
+            current.next=prev;
+            prev=current;
+            current=next;
+        }
+        return prev;
+    }
+}
